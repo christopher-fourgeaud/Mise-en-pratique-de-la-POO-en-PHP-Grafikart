896e0a1839fea34e380425a143bd05608be7d652
Christopher Fourgeaud <christopher.fourgeaud@gmail.com>
2020-03-23 19:25:55 +0100

    Mise en place du widget admin

--- src/Admin/config.php
+++ src/Admin/config.php
@@ -1,6 +1,21 @@
 <?php
 
+use Framework\Router;
+use App\Admin\AdminModule;
+use App\Admin\AdminTwigExtension;
+use App\Admin\DashboardAction;
+
+use function DI\add;
+use function DI\autowire;
+use function DI\create;
+use function DI\get;
+use Framework\Renderer\RendererInterface;
+
 return [
     'admin.prefix' => '/admin',
+    'admin.widgets' => [],
+    AdminTwigExtension::class => autowire()->constructor(get('admin.widgets')),
+    AdminModule::class => autowire()->constructorParameter('prefix', get('admin.prefix')),
+    DashboardAction::class => autowire()->constructorParameter('widgets', get('admin.widgets')),
 
 ];
